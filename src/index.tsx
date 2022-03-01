1286acc0046e1c5e589867916e0d191b98e9375d
ivesyu <ivesyu@tencent.com>
2022-03-01 15:23:59 +0800

    feat: 添加page

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './page-forwardRef';
+// 这里修改page-xxx 来查看对应hooks使用方式
+import App from './page-redux';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
